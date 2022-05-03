6233cf9b46dcf823111164dbd14d89111e63bd69
Dennis Liu <dennis.liu9@gmail.com>
2022-05-03 19:04:15 +0000

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+// print 0 through 9
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+// print increments of 2 in 10 loops
+for (let i = 0; i <= 9 * 2; i += 2) {
+  console.log(i);
+}
+
+// count down from 100
+for (let i = 100; i >= 0; i -= 1) {
+  console.log('Time till explosion: ', i, '!');
+}
